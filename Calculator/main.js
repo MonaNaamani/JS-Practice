587fa2675db5ef1b51d5209eaf34b0944ee9cac4
Mona Naamani <134978312+MonaNaamani@users.noreply.github.com>
2023-07-19 23:47:07 +0300

    Create a folder for each exercise

--- Calculator/main.js
+++ Calculator/main.js
@@ -1,12 +1,18 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="UTF-8">
-  <title>Page Title</title>
-  <link rel="stylesheet" href="style.css"/>
-</head>
-<body>
-  <script>
-  </script>
-</body>
-</html>
+function calc() {
+  let a = parseInt(document.querySelector('#value1').value);
+  let b = parseInt(document.querySelector('#value2').value);
+  let op = document.querySelector('#operator').value;
+  let calculate;
+
+  if(op == 'add') {
+    calculate = a + b;
+  } else if(op == 'min') {
+    calculate = a - b;
+  } else if (op == 'div') {
+    calculate = a / b;
+  } else if (op == 'mul') {
+    calculate = a * b;
+  }
+
+  document.querySelector('#result').innerHTML = calculate;
+}
